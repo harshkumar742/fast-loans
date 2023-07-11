444392ce3a57940584e93dd11eec8b6801d019e3
Harsh K <harshkumar742@gmail.com>
2023-07-12 01:11:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import signal
 Base.metadata.create_all(bind=engine)
 
 # Create a new FastAPI instance
-# test comment5
+# test comment6
 app = FastAPI()
 
 # Add CORS middleware to allow cross-origin requests
